81179f059529d5ebd13d800891b7ecfa2534da79
everm1nd <everm1nd.mail@gmail.com>
2016-04-22 11:42:01 +0200

    implemented sound engine prototype

---
+++ src/js/main.js
@@ -0,0 +1,27 @@
+var osc;
+var freq = 500;
+var volume = 0;
+
+function setup() {
+  createCanvas(500, 500);
+  backgroundColor = color(255,0,255);
+  textAlign(CENTER);
+
+  osc = new p5.Oscillator();
+  osc.setType('sine');
+  osc.freq(freq);
+  osc.amp(volume);
+  osc.start();
+}
+
+function draw() {
+  background(backgroundColor);
+  osc.freq(freq);
+  osc.amp(volume);
+}
+
+function mouseMoved() {
+  freq = map(mouseX,0,width,200,15000);
+  volume = map(mouseY,0,height,0,1);
+  console.log(freq, volume);
+}
