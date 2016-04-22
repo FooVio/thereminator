55b020e209c04ff3c9009572cbcdbe285909a8b2
Gaetano Contaldi <gaetanocontaldi000@gmail.com>
2016-04-22 19:13:34 +0200

    massive bass

--- src/js/instruments/bass.js
+++ src/js/instruments/bass.js
@@ -9,12 +9,17 @@ var socket = io();
 
 var freqIndex = 0;
 
-function setValues(e) {
-  if (e.data.length !== 0) {
-    freqIndex = Math.round(map(e.data[0].x,0,width, PENTATONIC_SCALE.length, 0));
+var kick = e.data.find(function(event) {
+  event.color == 'magenta';
+});
+
+function checkType(index) {
+  if(index) {
+    freqIndex = Math.round(map(e.data[index].x,0,width, PENTATONIC_SCALE.length, 0));
     freq = PENTATONIC_SCALE[freqIndex];
 
-    volume = map(e.data[0].y, 0, height, MAXVOLUME, 0);
+    volume = map(e.data[index].y, 0, height, MAXVOLUME, 0);
+
   } else {
     volume = 0;
   }
@@ -25,8 +30,25 @@ function setValues(e) {
     value: volume,
     freq: PENTATONIC_SCALE[freqIndex]
   };
-  console.log(data);
+
   socket.emit('client-message', data);
+}
+
+function setValues(e) {
+  if (e.data.length !== 0) {
+    console.log(e.data);
+    var magentaIndex = e.data.findIndex(function(ev) {
+      ev.color === 'magenta'
+    });
+    if(magentaIndex !== -1) {
+      checkType(magentaIndex);
+    }
+    var cyanIndex = e.data.findIndex(function(ev) {
+      ev.color === 'cyan'
+    });
+    if(cyanIndex !== -1) {
+      checkType(cyanIndex);
+    }
 }
 
 window.pubSub.subscribe(setValues);
