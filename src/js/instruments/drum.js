ff4087ead914e01ccbbf63d035326ec679d58887
everm1nd <everm1nd.mail@gmail.com>
2016-04-22 19:28:09 +0200

    drumssssss

--- src/js/instruments/drum.js
+++ src/js/instruments/drum.js
@@ -3,17 +3,49 @@ var freq = 500;
 var volume = 0;
 var MAXVOLUME = 0.5;
 
-var PENTATONIC_SCALE = [183.54, 231.246, 275.0, 367.08, 462.494, 617.354, 734.162, 924.986, 1234.708, 1468.324, 1959.978, 2469.416, 2936.648, 3919.954, 4938.834, 6592.552, 7839.908, 9877.666000000001, 13185.101999999999, 15679.818];
+var PENTATONIC_SCALE = [183.54, 231.246, 275.0, 367.08, 462.494, 617.354, 734.162];
 
 var socket = io();
 
-function sendEvent(e) {
-  if (e.data.length !== 0) {
-    freqIndex = Math.round(map(e.data[0].x,0,width, 0, PENTATONIC_SCALE.length));
-    volume = map(e.data[0].y,0,height,0,MAXVOLUME);
+var freqIndex = 0;
+
+function checkType(e, index, instrument) {
+  if(index!==-1) {
+    freqIndex = Math.round(map(e.data[index].x,0,width, PENTATONIC_SCALE.length, 0));
+    freq = PENTATONIC_SCALE[freqIndex];
+
+    volume = map(e.data[index].y, 0, height, MAXVOLUME, 0);
+    // volume = 1
+
   } else {
     volume = 0;
-  }
-}
+  };
 
-window.pubSub.subscribe(sendEvent);
+  data = {
+    instrument: instrument,
+    type: 'drum',
+    value: volume,
+    freq: PENTATONIC_SCALE[freqIndex]
+  };
+
+  console.log(data);
+
+  socket.emit('client-message', data);
+};
+
+function setValues(e) {
+  if (e.data.length !== 0) {
+    var magentaIndex = e.data.findIndex(function(ev) {
+      return ev.color === 'magenta'
+    });
+    // if(magentaIndex !== -1) {
+      checkType(e, magentaIndex, 'kick');
+    // }
+    var cyanIndex = e.data.findIndex(function(ev) {
+      return ev.color === 'cyan'
+    });
+    checkType(e, cyanIndex, 'snare');
+  }
+};
+
+window.pubSub.subscribe(setValues);
