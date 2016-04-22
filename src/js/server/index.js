fa65c09e8a50359e8f880d87f93bba359f4514af
everm1nd <everm1nd.mail@gmail.com>
2016-04-22 17:10:42 +0200

    added simple client-server routing for events

--- src/js/server/index.js
+++ src/js/server/index.js
@@ -5,14 +5,20 @@ var io = require('socket.io')(http);
 var path = require('path')
 console.log(path.join(__dirname, '../..'));
 
-app.use(express.static(path.join(__dirname, '../..')));
+app.use(express.static(path.join(__dirname, '../../..')));
 
 app.get('/', function(req, res){
-  res.sendFile(path.join(__dirname, '../..', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../..', 'index.html'));
 });
 
 io.on('connection', function(socket){
   console.log('a handsome user connected');
+  socket.on('client-message', function(msg){
+    io.emit('server-message', msg);
+  });
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, function(){
