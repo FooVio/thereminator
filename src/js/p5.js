288e8601c1d29a0bf35fd72c9d37fbf762ab670d
Kutay Kocayurt <kkocayurt@babbel.com>
2016-04-22 15:40:18 +0200

    plays D major pentatonic scale notes

--- src/js/p5.js
+++ src/js/p5.js
@@ -3,11 +3,15 @@ var freq = 500;
 var volume = 0;
 var MAXVOLUME = 0.5;
 
+var PENTATONIC_SCALE = [183.54, 231.246, 275.0, 367.08, 462.494, 617.354, 734.162, 924.986, 1234.708, 1468.324, 1959.978, 2469.416, 2936.648, 3919.954, 4938.834, 6592.552, 7839.908, 9877.666000000001, 13185.101999999999, 15679.818];
+
 function setValues(e) {
   console.log(1);
   // console.log(JSON.stringify(e));
   if (e.data[0]) {
-    freq = map(e.data[0].x,0,width,200,15000);
+    freqIndex = Math.round(map(e.data[0].x,0,width, 0, PENTATONIC_SCALE.length));
+    freq = PENTATONIC_SCALE[freqIndex];
+
     volume = map(e.data[0].y,0,height,0,MAXVOLUME);
   }
 }
