dceda3634b613ffec569b1016ca279b66f326fa4
everm1nd <everm1nd.mail@gmail.com>
2016-04-28 18:20:20 +0200

    implemented network framerate reduced amount of client->server messages close #17

--- src/js/instruments/oscillator.js
+++ src/js/instruments/oscillator.js
@@ -2,12 +2,25 @@ var osc;
 var freq = 500;
 var volume = 0;
 var MAXVOLUME = 1;
+var lastTime = 0;
+var MAXDELTA = 500;
 
 var PENTATONIC_SCALE = [183.54, 231.246, 275.0, 367.08, 462.494, 617.354, 734.162];
 
 var socket = io();
 
 var freqIndex = 0;
+
+function signal(data) {
+  var currentTime = new Date().getTime();
+  var delta = currentTime - lastTime;
+
+  if (delta > MAXDELTA) {
+    lastTime = currentTime;
+    console.log(data);
+    socket.emit('client-message', data);
+  }
+}
 
 function setValues(e) {
   if (e.data.length !== 0) {
@@ -25,8 +38,8 @@ function setValues(e) {
     value: volume,
     freq: PENTATONIC_SCALE[freqIndex]
   };
-  console.log(data);
-  socket.emit('client-message', data);
+
+  signal(data);
 }
 
 window.pubSub.subscribe(setValues);
