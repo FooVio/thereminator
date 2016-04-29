a2646dc0209d808eb8f30bc4e06cff2dfe368664
Gaetano Contaldi <gaetanocontaldi000@gmail.com>
2016-04-29 21:52:32 +0200

    update clients configuration

--- src/js/clients.js
+++ src/js/clients.js
@@ -21,6 +21,43 @@
           x: 'amp',
           y: 'freq'
         }
+      },
+      magenta: {
+        instrument: 'bass',
+        parameters: {
+          x: 'amp',
+          y: 'freq'
+        }
+      },
+      yellow: {
+        instrument: 'bass',
+        parameters: {
+          x: 'amp',
+          y: 'freq'
+        }
+      }
+    },
+    synth: {
+      cyan: {
+        instrument: 'synth',
+        parameters: {
+          x: 'amp',
+          y: 'freq'
+        }
+      },
+      magenta: {
+        instrument: 'synth',
+        parameters: {
+          x: 'amp',
+          y: 'freq'
+        }
+      },
+      yellow: {
+        instrument: 'synth',
+        parameters: {
+          x: 'amp',
+          y: 'freq'
+        }
       }
     }
   };
