b6368beb59e4f33ce0108b787ceafac18f01e923
Gaetano Contaldi <gaetanocontaldi000@gmail.com>
2016-04-29 17:24:27 +0200

    remove dead code

--- src/js/pubsub.js
+++ src/js/pubsub.js
@@ -1,10 +1,3 @@
-window.width = 450;
-window.height = 600;
-
-window.map = function(n, start1, stop1, start2, stop2) {
-  return ((n-start1)/(stop1-start1))*(stop2-start2)+start2;
-};
-
 window.pubSub = {
   listener: null,
   subscribe: function(fn) {
